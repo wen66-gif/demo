d43ffef2ebc012a6a08586776b0ef6a71ce48062
wen66-gif <2376190259@qq.com>
2021-09-18 20:58:50 +0800

    用户表信息

--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -5,7 +5,13 @@ public class User {
     private String name;
     private String pwd;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
